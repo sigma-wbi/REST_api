fd76cd563513125a73b42f5b9fc6da4b1b4b46ab
ayaan <rudehd2050@naver.com>
2023-01-21 14:12:46 +0900

    회원탈퇴기능 완(db도 정상적으로 지워짐)

--- django/common/forms.py
+++ django/common/forms.py
@@ -14,3 +14,27 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()  #  get_user_model() 해야되는 이유 찾기
         fields = UserCreationForm.Meta.fields + ("gender", "birthday", "phone")
+        widgets = {
+            'gender': forms.Select(attrs={'class':'form-control2'})
+        }
+
+
+from django.contrib.auth.hashers import check_password
+
+class CheckPasswordForm(forms.Form):
+    password = forms.CharField(label='비밀번호', widget=forms.PasswordInput(
+        attrs={'class': 'form-control',}), 
+    )
+    # 현재 접속중인 사용자의 password를 가져오기
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = user
+    # form에 입력된 password 값과 init으로 생성된 현재 사용자의 password 값을 django에서 제공하는 check_password를 통해 비교
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = self.user.password
+        
+        if password:
+            if not check_password(password, confirm_password):
+                self.add_error('password', '비밀번호가 일치하지 않습니다.')
--- django/common/views.py
+++ django/common/views.py
@@ -1,9 +1,10 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
-from django.contrib.auth import get_user_model
-from django.conf import settings
-from .models import User
+from django.contrib import messages
+# from django.contrib.auth import get_user_model
+# from django.conf import settings
+# from .models import User
 
 # User = get_user_model()
 # User = settings.AUTH_USER_MODEL
@@ -23,3 +24,22 @@ def signup(request):
     else:
         form = CustomUserCreationForm()
     return render(request, "common/signup.html", {"form": form})
+
+
+from .forms import CheckPasswordForm
+from django.contrib.auth.decorators import login_required
+
+@login_required # 로그인하지 않은 사용자의 접근을 막기 위해 decorator를 추가
+def delete_view(request):
+    if request.method == 'POST':
+        password_form = CheckPasswordForm(request.user, request.POST)
+        
+        if password_form.is_valid():
+            request.user.delete() # delete()로 DB에서 현재 user를 삭제하고 logout()을 통해 세션을 만료
+            logout(request)
+            messages.success(request, "회원탈퇴가 완료되었습니다.")
+            return redirect('/common/login/')
+    else:
+        password_form = CheckPasswordForm(request.user)
+
+    return render(request, 'common/delete.html', {'password_form':password_form})
